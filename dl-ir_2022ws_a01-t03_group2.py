4dd8a32537a9309939fe1f84f5df905158ae3de9
Andreas Schneider <andreas@yourdisposal.de>
2022-11-03 21:13:27 +0100

    exercise part b and c

--- dl-ir_2022ws_a01-t03_group2.py
+++ dl-ir_2022ws_a01-t03_group2.py
@@ -83,3 +83,37 @@ plt.bar(x, y[0])
 plt.bar(x, y[1], bottom=y[0])
 
 plt.show()
+
+
+# exercise part b) chart of wordcount in first part
+rows_by_wordcount_before_first_colon = {}
+for i, row in enumerate(data_rows):
+    title = row[2]
+    # match any title with a colon after word characters followed by another word character
+    # if re.match(r'.*\w.*:.*\w', title):
+    # had to make it anything BUT colon because row 191969 ['article', '2012', ': onastre: OER Development and Promotion. Outcomes of an International Research Projecton the OpenCourseWare Model.']
+    if re.match(r'[^:]*\w[^:]*:.*\w', title):
+        text_before_first_colon = title.split(':')[0]
+        words_before_first_colon = re.findall(r'\w+', text_before_first_colon)
+        wordcount = len(words_before_first_colon)
+        rows_by_wordcount_before_first_colon[wordcount] = rows_by_wordcount_before_first_colon.get(wordcount, [])
+        rows_by_wordcount_before_first_colon[wordcount].append((i, row))
+
+# row 25714 striked out as 72 words before first colon ['article', '2017', "Maryanthe Malliaris and Saharon Shelah, Cofinality spectrum problems in model theory, set theory and general topology . Journal of the American Mathematical Society, vol. 29 (2016), pp. 237-297. - Maryanthe Malliaris and Saharon Shelah, Existence of optimal ultrafilters and the fundamental complexity of simple theories. Advances in Mathematics, vol. 290 (2016), pp. 614-681. - Maryanthe Malliaris and Saharon Shelah, Keisler's order has infinitely many classes. Israel Journal of Mathematics, to appear, https://math.uchicago.edu/∼mem/."]
+# URLs or generally a colon that's not part of a sentence structure should be filtered
+# a fine finding for exercise part c. Let's ignore that for now
+
+# get x-values
+x = range(min(rows_by_wordcount_before_first_colon), max(rows_by_wordcount_before_first_colon) + 1)
+
+# get y-values
+y = []
+for col in x:
+    if col in rows_by_wordcount_before_first_colon:
+        y.append(len(rows_by_wordcount_before_first_colon[col]))
+    else:
+        y.append(0)
+
+plt.bar(x, y)
+
+plt.show()
